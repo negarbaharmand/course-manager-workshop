0551fe120595929e576f3a700c3062071c948670
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-23 10:26:02 +0200

    Implement Student class

--- src/main/java/se/lexicon/course_manager_assignment/model/Student.java
+++ src/main/java/se/lexicon/course_manager_assignment/model/Student.java
@@ -1,5 +1,72 @@
 package se.lexicon.course_manager_assignment.model;
 
-public class Student {
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Student implements Serializable {
+    private final int id;
+    private String name;
+    private String email;
+    private String address;
+
+
+    public Student(String name, String email, String address) {
+        this.id = StudentSequencer.nextStudentId();
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name) && Objects.equals(email, student.email) && Objects.equals(address, student.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
--- src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -1,4 +1,15 @@
 package se.lexicon.course_manager_assignment.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class StudentTest {
+
+    @Test
+    public void testIdGenerator() {
+        Student s1 = new Student("Negar", "negar@gmail.com", "fdsfd");
+        Student s2 = new Student("Basim", "basim@basim.com", "");
+        assertNotEquals(s1.getId(), s2.getId());
+    }
 }
