423f9414d7035dbe7587c45507e120ae4b63997b
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-23 15:06:02 +0200

    Write test for Student class

--- src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -1,15 +1,30 @@
 package se.lexicon.course_manager_assignment.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class StudentTest {
+    private Student s1;
+    private Student s2;
+    private Student s3;
+
+    @BeforeEach
+    public void setUp() {
+         s1 = new Student("Negar", "negar@gmail.com", "fdsfd");
+         s2 = new Student("Basim", "basim@basim.com", "");
+         s3 = new Student("Negar", "negar@gmail.com", "fdsfd");
+    }
 
     @Test
     public void testIdGenerator() {
-        Student s1 = new Student("Negar", "negar@gmail.com", "fdsfd");
-        Student s2 = new Student("Basim", "basim@basim.com", "");
         assertNotEquals(s1.getId(), s2.getId());
     }
+
+    @Test
+    public void testHashcode() {
+        assertNotEquals(s1.hashCode(), s3.hashCode());
+    }
+
 }
