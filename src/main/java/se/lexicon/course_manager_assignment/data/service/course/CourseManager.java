32f34204a321d0f6f42da7ae6553dcac2602cae4
Negar <negarbaharmand@outlook.com>
2023-12-20 13:15:24 +0100

    Implement methods in CourseManager

--- src/main/java/se/lexicon/course_manager_assignment/data/service/course/CourseManager.java
+++ src/main/java/se/lexicon/course_manager_assignment/data/service/course/CourseManager.java
@@ -8,10 +8,14 @@ import se.lexicon.course_manager_assignment.data.service.converter.Converters;
 import se.lexicon.course_manager_assignment.dto.forms.CreateCourseForm;
 import se.lexicon.course_manager_assignment.dto.forms.UpdateCourseForm;
 import se.lexicon.course_manager_assignment.dto.views.CourseView;
+import se.lexicon.course_manager_assignment.model.Course;
+import se.lexicon.course_manager_assignment.model.Student;
 
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseManager implements CourseService {
@@ -29,56 +33,105 @@ public class CourseManager implements CourseService {
 
     @Override
     public CourseView create(CreateCourseForm form) {
-        return null;
+        if (form == null) {
+            throw new IllegalArgumentException("The form cannot be null.");
+        }
+        Course createdCourse = courseDao.createCourse(form.getCourseName(), form.getStartDate(), form.getWeekDuration());
+        return converters.courseToCourseView(createdCourse);
     }
 
     @Override
     public CourseView update(UpdateCourseForm form) {
-        return null;
+        if (form == null) {
+            throw new IllegalArgumentException("The update course form cannot be null.");
+        }
+        Course toUpdateCourse = courseDao.findById(form.getId());
+        if (toUpdateCourse == null) {
+            throw new IllegalArgumentException("Course not found with ID: " + form.getId());
+        }
+        toUpdateCourse.setCourseName(form.getCourseName());
+        toUpdateCourse.setStartDate(form.getStartDate());
+        toUpdateCourse.setWeekDuration(form.getWeekDuration());
+
+        courseDao.removeCourse(toUpdateCourse);  //Remove the course from database
+        courseDao.createCourse(toUpdateCourse.getCourseName(), toUpdateCourse.getStartDate(), toUpdateCourse.getWeekDuration());  //Create a new entity
+
+        return converters.courseToCourseView(toUpdateCourse);
     }
+
 
     @Override
     public List<CourseView> searchByCourseName(String courseName) {
-        return null;
+        Collection<Course> courses = courseDao.findByNameContains(courseName);
+        return courses.stream()
+                .map(converters::courseToCourseView)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<CourseView> searchByDateBefore(LocalDate end) {
-        return null;
+        Collection<Course> courses = courseDao.findByDateAfter(end);
+        return courses.stream()
+                .map(converters::courseToCourseView)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<CourseView> searchByDateAfter(LocalDate start) {
-        return null;
+        Collection<Course> courses = courseDao.findByDateAfter(start);
+        return courses.stream()
+                .map(converters::courseToCourseView)
+                .collect(Collectors.toList());
     }
 
     @Override
     public boolean addStudentToCourse(int courseId, int studentId) {
+        Course course = courseDao.findById(courseId);
+        Student student = studentDao.findById(studentId);
+
+        if (course != null && student != null) {
+            return course.enrollStudent(student);
+        }
         return false;
     }
 
     @Override
     public boolean removeStudentFromCourse(int courseId, int studentId) {
+        Course course = courseDao.findById(courseId);
+        Student student = studentDao.findById(studentId);
+
+        if (course != null && student != null) {
+            return course.unrollStudent(student);
+        }
         return false;
     }
 
     @Override
     public CourseView findById(int id) {
-        return null;
+        Course course = courseDao.findById(id);
+        return converters.courseToCourseView(course);
     }
 
     @Override
     public List<CourseView> findAll() {
-        return null;
+        Collection<Course> courses = courseDao.findAll();
+        return converters.coursesToCourseViews(courses);
     }
 
     @Override
     public List<CourseView> findByStudentId(int studentId) {
-        return null;
+        Collection<Course> courses = courseDao.findByStudentId(studentId);
+        return converters.coursesToCourseViews(courses);
     }
+
 
     @Override
     public boolean deleteCourse(int id) {
+        Course course = courseDao.findById(id);
+        if (course != null) {
+            return courseDao.removeCourse(course);
+        }
         return false;
     }
 }
+
